db26050cf9427267db99823b499d66929d9d58a3
Nothflare <pgp@nothflare.com>
2025-12-26 01:10:13 +0800

    feat: add SessionStart hook

---
+++ feature_tree/session_hook.py
@@ -0,0 +1,6 @@
+# feature_tree/session_hook.py
+#!/usr/bin/env python
+"""SessionStart hook - injects Feature Tree context."""
+
+print("This project uses Feature Tree for feature management.")
+print("Use /commit after completing work to commit + update the tree.")
